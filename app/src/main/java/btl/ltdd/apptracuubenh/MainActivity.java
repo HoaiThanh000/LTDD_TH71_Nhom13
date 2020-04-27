b2f991a0566e5c0a79e9a89bbea247c6439692e9
hoaithanh <1751010136thanh@ou.edu.vn>
2020-04-27 18:57:36 +0700

    app trang

---
+++ app/src/main/java/btl/ltdd/apptracuubenh/MainActivity.java
@@ -0,0 +1,14 @@
+package btl.ltdd.apptracuubenh;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
